75ddb36a835795239db38eeac1f4db489fdac739
Muhammaad-taraaz <muhammadtarazansari12@gmail.com>
2023-12-25 03:13:46 +0500

    Project in progress

--- Project/Backend/server.js
+++ Project/Backend/server.js
@@ -1,7 +1,35 @@
-const app = require("./app");
-// const dotenv = require("dotenv");
-// dotenv.config({ path: "./config/config.env" });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("server started port no :" + PORT);
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 5924;
+app.use(express.json());
+
+connectDatabase().then(e=>console.log("connected ss")).catch((e)=>console.log(e))
+async function connectDatabase(){
+    await mongoose.connect("mongodb+srv://taraz:taraz12@todos.g2clqpo.mongodb.net/test?retryWrites=true&w=majority")
+}
+
+const router = express.Router();
+app.use(router)
+
+
+const UserSchema = new mongoose.Schema({
+    username : {type:String},
+    password : {type:Boolean},
+    email : {type:string}
+})
+
+const UserModel = mongoose.model("user",UserSchema);
+
+router.get("/got", async (req,res) => {
+    const userList = await UserModel.find();
+    res.status(200).json({userList})
 });
+
+router.all("/", (req,res) => {
+    res.json({message : " Succesfully we are live 🥳🥳🚀🚀🎥"})
+})
+app.listen(PORT,()=> {
+    console.log("server started");
+})
--- Project/Frontend/src/Pages/data.js
+++ Project/Frontend/src/Pages/data.js
@@ -1,21 +1,182 @@
 export default [
     {
         id: 1,
-        question : "Javascript is an _____ language",
+        question : "What is the capital of France",
         options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural'
+            'Rome',
+            'Paris',
+            'Madrid'
         ]
     },
     {
         id: 2,
-        question : "Following Method...",
+        question : "What is the square root of 144",
         options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural'
+            '12',
+            '10',
+            '14'
         ]
     },
-    
+    {
+        id: 3,
+        question : "What is the largest planet in our solar system",
+        options : [
+            'Earth',
+            'Mars',
+            'Jupiter'
+        ]
+    },
+    {
+        id: 4,
+        question : "In which year did world war ll end",
+        options : [
+            '1943',
+            '1950',
+            '1945'
+        ]
+    },
+    {
+        id: 5,
+        question : "What is the capital of Japan",
+        options : [
+            'Beijing',
+            'Seoul',
+            'Tokyo'
+        ]
+    },
+    {
+        id: 6,
+        question : "How many continents are there in the world",
+        options : [
+            '5',
+            '7',
+            '9'
+        ]
+    },
+    {
+        id: 7,
+        question : "Who discovered penicillin",
+        options : [
+            'Alexander Fleming ',
+            'Marie curie',
+            'Albert Einstein'
+        ]
+    },
+    {
+        id: 8,
+        question : "What is the chemical symbol for gold",
+        options : [
+            'Gd',
+            'Au',
+            'Ag'
+        ]
+    },
+    {
+        id: 9,
+        question : "How many sides does a Hexagon have",
+        options : [
+            '6',
+            '7',
+            '8'
+        ]
+    },
+    {
+        id: 10,
+        question : "Which planet is known as the Red Planet",
+        options : [
+            'Mars',
+            'Venus',
+            'Jupiter'
+        ]
+    },
+    {
+        id: 11,
+        question : "what is the capital of Australia",
+        options : [
+            'Canberra',
+            'Sydney',
+            'Melbourne'
+        ]
+    },  
+    {
+        id: 12,
+        question : "Who developed the theory of relativity",
+        options : [
+            'Issac Newton',
+            'Stephen Hawking',
+            'Albert Einstein'
+        ]
+    },  
+    {
+        id: 13,
+        question : "What is the powerhouse of the cell",
+        options : [
+            'Nucleus',
+            'Ribosome',
+            'Mitochondria'
+        ]
+    },  
+    {
+        id: 14,
+        question : "What is the square of 9",
+        options : [
+            '72',
+            '81',
+            '90'
+        ]
+    },  
+    {
+        id: 15,
+        question : "What is the chemical symbol for water",
+        options : [
+            'HO',
+            'HO2',
+            'O2'
+        ]
+    },  
+    {
+        id: 16,
+        question : "Who is known as the FATHER OF COMPUTERS",
+        options : [
+            'Alan Turing',
+            'Charles Babbage',
+            'Bill Gates'
+        ]
+    },  
+    {
+        id: 17,
+        question : "What is the next number in the sequence  2,6,12,20_____",
+        options : [
+            '24',
+            '30',
+            '28'
+        ]
+    }, 
+    {
+        id: 18,
+        question : "Which word doesnot belong in the following group  Apple,Banana,Orange,Grape",
+        options : [
+            'Pear',
+            'Apple',
+            'Carrot'
+        ]
+    }, 
+    {
+        id: 19,
+        question : "Thickest layer of Earth",
+        options : [
+            'Mantle',
+            'Subtle',
+            'Castle'
+        ]
+    }, 
+    {
+        id: 20,
+        question : "Income tax rate is 10% if tax deducted is 1500 what will be the income",
+        options : [
+            '150,000',
+            '15000',
+            '16500'
+        ]
+    },    
 ]
