c0a2e0ee29bde64efe240cea5f71d424a8f8d63e
Muhammaad-taraaz <muhammadtarazansari12@gmail.com>
2023-12-29 22:27:33 +0500

    Project in progress

--- Project/Backend/server.js
+++ Project/Backend/server.js
@@ -1,35 +1,144 @@
-const express = require("express");
-const mongoose = require("mongoose");
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 const PORT = 5924;
-app.use(express.json());
 
+// Connect to MongoDB
 connectDatabase().then(e=>console.log("connected ss")).catch((e)=>console.log(e))
-async function connectDatabase(){
-    await mongoose.connect("mongodb+srv://taraz:taraz12@todos.g2clqpo.mongodb.net/test?retryWrites=true&w=majority")
-}
+  
+  async function connectDatabase(){
+  await mongoose.connect("mongodb+srv://taraz:taraz12@todos.g2clqpo.mongodb.net/test?retryWrites=true&w=majority")}
 
-const router = express.Router();
-app.use(router)
-
-
-const UserSchema = new mongoose.Schema({
-    username : {type:String},
-    password : {type:Boolean},
-    email : {type:string}
-})
-
-const UserModel = mongoose.model("user",UserSchema);
-
-router.get("/got", async (req,res) => {
-    const userList = await UserModel.find();
-    res.status(200).json({userList})
+// Define User Schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
 });
 
+// Define User Model
+const User = mongoose.model('User', userSchema);
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+const router = express.Router();
+
+// Signup
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    
+    // Hash the password before saving it
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error creating user');
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).send('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).send('Invalid credentials');
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error finding user');
+  }
+});
+
+// Get all users
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching users');
+  }
+});
+
+// Get a user by ID
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching user');
+  }
+});
+
+// Update a user by ID
+router.put('/users/:id', async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedUser) {
+      return res.status(404).send('User not found');
+    }
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error updating user');
+  }
+});
+
+// Delete a user by ID
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res.status(404).send('User not found');
+    }
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error deleting user');
+  }
+});
+
+// Use routes
+app.use('/api', router);
+
+// Start the server
 router.all("/", (req,res) => {
-    res.json({message : " Succesfully we are live 🥳🥳🚀🚀🎥"})
-})
-app.listen(PORT,()=> {
-    console.log("server started");
-})
+        res.json({message : " Succesfully we are live 🥳🥳🚀🚀🎥"})
+    })
+    app.listen(PORT,()=> {
+        console.log("server started");
+    })
+    
--- Project/Frontend/src/main.jsx
+++ Project/Frontend/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import Router from  "./Router/Router"
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
